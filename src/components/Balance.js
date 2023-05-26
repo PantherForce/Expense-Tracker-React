707d1fbf17ec123ab5fbca9cf955eba7b435ced0
likhithreddy28 <likhithreddy150@gmail.com>
2023-05-26 20:47:52 +0530

    mm

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,8 +4,8 @@ export const Balance = () => {
     return (
         <>
            <center>
-         <h3> Balance</h3>
-         <h2 id='balance'>$00 .00 </h2>
+         <h3> Balance Amount</h3>
+         <h2 id='balance'>$00.00 </h2>
          </center>
         </>
     )
